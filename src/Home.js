9aab702c054b31437badb5663935f650969a32d5
Wizschool <wizschool@Wizschoolui-iMac.local>
2021-11-11 17:37:27 +0900

    product info set

--- src/Home.js
+++ src/Home.js
@@ -11,67 +11,79 @@ function Home() {
 
             <div class="home__row">
                 <Product 
-                    id="" 
+                    id="12345" 
                     title="All-new Echo Dot (4th generation) International Version"
                     price={49.99} 
                     image="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/January/Fuji_Dash_EchoDot_Jan21_1x._SY304_CB413050328_.jpg"
                     rating={5}
                 />
                 <Product
-                    id="" 
-                    title="" 
-                    price={} 
-                    image="" 
-                    rating={4}
+                    id="23456" 
+                    title="Oculus Quest 2" 
+                    price={299.0} 
+                    image="https://m.media-amazon.com/images/I/615YaAiA-ML._SL1500_.jpg" 
+                    rating={5}
                 />
                 <Product
-                    id="" 
-                    title="" 
-                    price={} 
-                    image="" 
-                    rating={4}
+                    id="25647" 
+                    title="Nintendo Switch - Black" 
+                    price={355.56} 
+                    image="https://m.media-amazon.com/images/I/61RtiOhejgL._AC_SL1500_.jpg" 
+                    rating={5}
                 />
                 <Product
-                    id="" 
-                    title="" 
-                    price={} 
-                    image="" 
-                    rating={4}
+                    id="23367" 
+                    title="Wireless Gaming Mouse, Computer Mouse with Beetle Shape" 
+                    price={17.99} 
+                    image="https://m.media-amazon.com/images/I/61ogOp8NKhL._AC_SL1500_.jpg" 
+                    rating={3}
                 />
 
             </div>
 
             <div class="home__row">
             <Product
-                    id="" 
-                    title="" 
-                    price={} 
-                    image="" 
+                    id="567432" 
+                    title="Hbada Gaming Chair Ergonomic Racing Chair " 
+                    price={169.99} 
+                    image="https://m.media-amazon.com/images/I/71IC-JS4p1L._AC_SL1500_.jpg" 
                     rating={4}
                 />
                 <Product
-                    id="" 
-                    title="" 
-                    price={} 
-                    image="" 
-                    rating={4}
+                    id="234255" 
+                    title="Redragon S101 Wired Gaming Keyboard and Mouse Combo RGB Backlit Gaming Keyboard" 
+                    price={31.95} 
+                    image="https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_SL1500_.jpg" 
+                    rating={3}
                 />
                 <Product
-                    id="" 
-                    title="" 
-                    price={} 
-                    image="" 
+                    id="556884" 
+                    title="Vornado EVDC500 Energy Smart Evaporative Humidifier with Automatic Shut-off" 
+                    price={128.92} 
+                    image="https://m.media-amazon.com/images/I/81or1vvkQPL._AC_SL1500_.jpg" 
                     rating={4}
                 />
             </div>
 
             <div class="home__row">
-            <Product />
+                <Product
+                    id="559984" 
+                    title="Introducing Amazon Fire TV 50"
+                    price={329.99} 
+                    image="https://m.media-amazon.com/images/I/51EPd38RHQL._AC_SL1000_.jpg" 
+                    rating={4}
+                />
 
             </div>
             
             <div class="home__row">
-            <Product />
+                <Product
+                    id="449984" 
+                    title="Canon EOS Rebel T7 DSLR Camera with 18-55mm Lens"
+                    price={419.99} 
+                    image="https://m.media-amazon.com/images/I/71EWRyqzw0L._AC_SL1500_.jpg" 
+                    rating={4}
+                />
             </div>
 
         </div>
