651f4dbcb81f779c73cf7d6865496e17f141a2a9
Wizschool <wizschool@Wizschoolui-iMac.local>
2021-11-12 17:54:20 +0900

    firebase user authentication set up

--- src/Signin.js
+++ src/Signin.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Signin.css";
 import {Link} from 'react-router-dom';
+import {auth} from './firebase';
 
 function Signin() {
+    const [email, setsEmail] = useState('');
+    const [password, setsPassword] = useState('');
+    const logIn = e => {
+        //prevent the page from refreshing
+        e.preventDefault();
+
+        //firebase login
+    }
+    const register = e => {
+        e.preventDefault();
+
+        auth.createUserWithEmailAndPassword(email, password)
+        .then((auth) => {
+            //successfully created a new user with email and password
+            console.log(auth);
+
+        })
+        .catch(error => alert(error.message));
+
+        //firebase register
+    }
+
     return (
         <div className="signIn">
 
@@ -15,17 +38,21 @@ function Signin() {
 
                 <form>
                     <h5>Email</h5>
-                    <input type="text"></input>
+                    <input type="text" value={email} 
+                    onChange={e => setsEmail(e.target.value)} />
+
                     <h5>Password</h5>
-                    <input type="password"></input>
-                    <button className="signIn__button">Sign In</button>
+                    <input type="password" value={password} 
+                    onChange={e => setsPassword(e.target.value)} />
+
+                    <button type="submit" onClick={logIn} className="signIn__button">Sign In</button>
 
                     <p>By signing-in, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
                 </form>
 
             </div>
 
-            <button className="signIn__registerButton">Create your Amazon account</button>
+            <button onClick={register} className="signIn__registerButton">Create your Amazon account</button>
         </div>
     )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,9 @@
+import {initializeApp} from 'firebase/app';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBF_Lfzozm5W2mJS7LGisaI2HCEzZFSQYI",
@@ -8,3 +14,12 @@ const firebaseConfig = {
     appId: "1:319429616973:web:a54e89e38373bc1dd78304",
     measurementId: "G-96FPWRRQMN"
   };
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {db, auth};
+
